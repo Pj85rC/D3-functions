9a6dfc8f2a8adf778160dc7c1a2d9d5f0b33b12f
unknown <patricio.ramirez.c@gmail.com>
2022-07-18 18:51:31 -0400

    uploading

---
+++ assets/js/1function.js
@@ -0,0 +1,7 @@
+
+//Function Expression:
+const fExpresion = function(a=1, b=2, c=3){
+    const suma = a + b + c;
+    console.log( `El valor de la suma expresado en esta Función de Expresión es: ${suma}` );
+}
+fExpresion();
---
+++ assets/js/2arrow.js
@@ -0,0 +1,4 @@
+//Arrow Function:
+const suma = (a=500, b=1000) => a + b;
+console.log( `El valor de la suma expresado en esta Función de Flecha es: ${suma()}` );
+suma();
